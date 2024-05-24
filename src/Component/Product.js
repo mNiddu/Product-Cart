5bece40b758e5678f48c4f522a134da7ce2d3a8f
mNiddu <mniddu19@gmail.com>
2024-05-24 18:07:53 +0530

    24-05/2

---
+++ src/Component/Product.js
@@ -0,0 +1,62 @@
+import Cam from './image/cam.png'
+import Bottile from './image/bottle.png'
+import Jrink from './image/jrink.png'
+import Minicam from './image/minicam.png'
+import Perrier from './image/perrier.png'
+import Sound from './image/sound.png'
+export const Product =[
+    {
+        id:1,
+        product_name:"Camera",
+        price:20000,
+        image:Cam,
+        desc:` 
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur commodi sed eaque tenetur dolorum perspiciatis! At laborum ut voluptatum incidunt fugit neque autem quidem nihil consequatur, unde molestias, hic placeat.
+        `
+},
+    {
+        id:2,
+        product_name:"Bottile",
+        price:2000,
+        image:Bottile,
+        desc:` 
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur commodi sed eaque tenetur dolorum perspiciatis! At laborum ut voluptatum incidunt fugit neque autem quidem nihil consequatur, unde molestias, hic placeat.
+        `
+},
+    {
+        id:3,
+        product_name:"Jrink",
+        price:1550,
+        image:Jrink,
+        desc:` 
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur commodi sed eaque tenetur dolorum perspiciatis! At laborum ut voluptatum incidunt fugit neque autem quidem nihil consequatur, unde molestias, hic placeat.
+        `
+},
+    {
+        id:4,
+        product_name:"Minicam",
+        price:25000,
+        image:Minicam,
+        desc:` 
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur commodi sed eaque tenetur dolorum perspiciatis! At laborum ut voluptatum incidunt fugit neque autem quidem nihil consequatur, unde molestias, hic placeat.
+        `
+},
+    {
+        id:5,
+        product_name:"Perrier",
+        price:1000,
+        image:Perrier,
+        desc:` 
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur commodi sed eaque tenetur dolorum perspiciatis! At laborum ut voluptatum incidunt fugit neque autem quidem nihil consequatur, unde molestias, hic placeat.
+        `
+},
+    {
+        id:6,
+        product_name:"Sound",
+        price:2000,
+        image:Sound,
+        desc:` 
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur commodi sed eaque tenetur dolorum perspiciatis! At laborum ut voluptatum incidunt fugit neque autem quidem nihil consequatur, unde molestias, hic placeat.
+        `
+},
+]
